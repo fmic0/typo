56c4f5074e3561000473f6d4977aeda69bd487a6
fmic0 <fmic_nz@yahoo.co.nz>
2013-08-22 22:09:28 +1200

    fin

--- app/models/article.rb
+++ app/models/article.rb
@@ -51,19 +51,19 @@ class Article < Content
     article = Article.find(@merge_with)
     if !article.nil?
 
-      text = body + article.body
-      self.body += article.body
-      ##self.save!
-      if  !article.comments.empty? 
-           article.comments.each do |comment|
-              comment.article_id = self.id
-      ##        comment.save! 
-           end 
-      end
-      ##article.delete
-      return self
-    end
-    nil
+       text = body + article.body
+       self.body += article.body
+       ##self.save!
+       if  !article.comments.empty? 
+            article.comments.each do |comment|
+               comment.article_id = self.id
+       ##        comment.save! 
+            end 
+       end
+       ##article.delete
+       return self
+     end
+     nil
   end
   # edx ---
 
