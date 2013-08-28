f9c164593995ace1229338d4330542bb6e147469
fmic0 <fmic_nz@yahoo.co.nz>
2013-08-28 12:20:15 +1200

    Update categories_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -26,9 +26,8 @@ class Admin::CategoriesController < Admin::BaseController
   def new_or_edit
     @categories = Category.find(:all)
     # edx fix --- error when a new category is selected
-    # @category = Category.find(params[:id])
-
-    @category = (params[:id].nil?) ? Category.new() : Category.find(params[:id])
+    @category = Category.find(params[:id])
+    # @category = (params[:id].nil?) ? Category.new() : Category.find(params[:id])
     # edx fix
     @category.attributes = params[:category]
     if request.post?
